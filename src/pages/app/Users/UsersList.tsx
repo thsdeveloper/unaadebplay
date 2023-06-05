5440807b4bdb298a4b09378b9df16cb03ae517ca
Thiago Pereira <ths.pereira@gmail.com>
2023-06-05 09:43:15 -0300

    Feature Youtube

--- src/pages/app/Users/UsersList.tsx
+++ src/pages/app/Users/UsersList.tsx
@@ -1,29 +1,24 @@
-import React, {useState, useEffect, useContext} from 'react';
-import {FlatList, TouchableOpacity} from 'react-native';
-import {Box, Text, VStack, Divider, Spinner, Stack, Icon, Input, HStack, Center} from 'native-base';
-import {getItems} from '../../../services/items';
-import {UserTypes} from '../../../types/UserTypes'; // Defina o tipo do usuário aqui
+import React, {useState, useCallback, useEffect} from 'react';
+import { FlatList, TouchableOpacity } from 'react-native';
+import { Box, Text, VStack, Divider, Spinner, Stack, Icon, Input, HStack, Center } from 'native-base';
 import { FontAwesome } from '@expo/vector-icons';
-import colors from "../../../constants/colors";
-import {getUsers} from "../../../services/user";
+import { getUsers } from "../../../services/user";
 import UserItem from "../../../components/UserItem";
-import {LoadingLottier} from "../../../components/LoadingLottier";
+import { LoadingLottier } from "../../../components/LoadingLottier";
 
-const PAGE_SIZE = 20; // Defina o número de usuários para carregar por vez
+const PAGE_SIZE = 20;
 
-const UserListPage = ({navigation}) => {
-    const [users, setUsers] = useState<UserTypes[]>([]);
+const UserListPage = ({ navigation }) => {
+    const [users, setUsers] = useState([]);
+    const [search, setSearch] = useState();
+    const [inputText, setInputText] = useState('');
+    const [page, setPage] = useState(1);
     const [isLoadingList, setIsLoadingList] = useState(false);
     const [isLoadingUser, setIsLoadingUser] = useState(false);
-    const [search, setSearch] = useState('');
-    const [page, setPage] = useState(1);
 
-    useEffect(() => {
-        loadUsers();
-    }, [search, page]);
-
-    const loadUsers = async () => {
+    const loadUsers = useCallback(async (reset = false) => {
         setIsLoadingList(true);
+
         const filters = search ? {
             filter: {
                 first_name: {
@@ -34,28 +29,43 @@ const UserListPage = ({navigation}) => {
 
         const responseUsers = await getUsers({
             ...filters,
-            limit: PAGE_SIZE, // Limite os resultados retornados para o tamanho da página
-            offset: (page - 1) * PAGE_SIZE, // O offset é a quantidade de resultados a pular
+            limit: PAGE_SIZE,
+            offset: reset ? 0 : (page - 1) * PAGE_SIZE,
         });
-        console.log('BUSCOU MAIS >>>')
 
-        setUsers(prevUsers => [...prevUsers, ...responseUsers]); // Adicione os novos usuários à lista existente
+        if (reset) {
+            setUsers(responseUsers);
+        } else {
+            setUsers(prevUsers => [...prevUsers, ...responseUsers]);
+        }
+
         setIsLoadingList(false);
-    };
+    }, [search, page]);
 
-    const handleUserPress = async (user: UserTypes) => {
+
+    const handleUserPress = async (user) => {
         setIsLoadingUser(true);
-        navigation.navigate('Dashboard', {screen: 'UserProfile', params: {id: user.id}});
+        navigation.navigate('Dashboard', { screen: 'UserProfile', params: { id: user.id } });
         setIsLoadingUser(false);
     };
 
     const handleLoadMore = () => {
-        setPage(prevPage => prevPage + 1); // Quando o usuário chega ao final da lista, carregue a próxima página
+        setPage(prevPage => prevPage + 1);
+    };
+
+    const handleSearch = () => {
+        setSearch(inputText);
+        setPage(1);
+        loadUsers(true);
     };
 
     const renderItem = ({ item }) => (
         <UserItem item={item} handleUserPress={handleUserPress} />
     );
+
+    useEffect(() => {
+        loadUsers();
+    }, [page, loadUsers]);
 
     return (
         <Box flex={1} bg="white">
@@ -63,10 +73,10 @@ const UserListPage = ({navigation}) => {
                 <Input
                     flex={1}
                     placeholder="Digite o nome do usuário"
-                    onChangeText={text => setSearch(text)}
-                    value={search}
+                    onChangeText={setInputText}
+                    value={inputText}
                     InputRightElement={
-                        <TouchableOpacity onPress={loadUsers}>
+                        <TouchableOpacity onPress={handleSearch}>
                             <Icon as={FontAwesome} name="search" size="sm" m={2} />
                         </TouchableOpacity>
                     }
@@ -75,13 +85,13 @@ const UserListPage = ({navigation}) => {
 
             {isLoadingList ? (
                 <LoadingLottier />
-            ) : users?.length > 0 ? (
+            ) : users.length > 0 ? (
                 <FlatList
                     data={users}
                     renderItem={renderItem}
                     keyExtractor={(item) => item.id.toString()}
-                    onEndReached={handleLoadMore} // Carregue mais usuários quando o usuário chegar ao final da lista
-                    onEndReachedThreshold={0.5} // Comece a carregar quando o usuário estiver a meio caminho de alcançar o final da lista
+                    onEndReached={handleLoadMore}
+                    onEndReachedThreshold={0.5}
                 />
             ) : (
                 <Center>
@@ -89,9 +99,7 @@ const UserListPage = ({navigation}) => {
                 </Center>
             )}
 
-            {isLoadingUser  && (
-                <LoadingLottier />
-            )}
+            {isLoadingUser && <LoadingLottier />}
         </Box>
     );
 };
--- src/pages/app/Youtube/index.tsx
+++ src/pages/app/Youtube/index.tsx
@@ -1,21 +1,73 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import React, {useCallback, useEffect, useRef, useState} from "react";
+import YoutubeIframe from "react-native-youtube-iframe";
+import {Text, Box, ScrollView, Button, Heading} from "native-base";
+import LottieView from "lottie-react-native";
+import colors from "../../../constants/colors";
+import {ActivityIndicator} from "react-native";
+import * as ScreenOrientation from "expo-screen-orientation"
+import {getItem, getItems} from "../../../services/items";
+import {RepertoriesTypes} from "../../../types/RepertoriesTypes";
 
 const YoutubePage = () => {
+    const animation = useRef(null);
+    const [videoReady, setVideoReady] = useState(false);
+    const [youtube, setYoutube] = useState();
+
+
+    useEffect(() => {
+        const loadInfoYoutube = async () => {
+            const responseYoutube = await getItems('youtube');
+            setYoutube(responseYoutube);
+        };
+
+        loadInfoYoutube();
+    }, []);
+
+
+
+    const onFullScreenChange = useCallback((isFullScreen: boolean) => {
+        if(isFullScreen){
+            ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE)
+        }else{
+            ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT)
+        }
+        console.log(isFullScreen)
+    }, [])
+
+
     return (
-        <View style={styles.center}>
-            <Text>Youtube Page</Text>
-        </View>
+        <ScrollView backgroundColor={colors.line} flex={1} px={6}>
+
+            <Box>
+                <Box alignItems="center" justifyContent="center">
+                    <LottieView
+                        autoPlay
+                        loop={false}
+                        ref={animation}
+                        style={{
+                            width: '100%',
+                            height: 150,
+                            aspectRatio: 1,
+                        }}
+                        source={youtube?.icone}
+                    />
+                    <Heading fontSize={26} fontWeight={"bold"} textAlign={"center"} color={colors.text}>{youtube?.title}</Heading>
+                    <Text textAlign={"center"} color={colors.text}>{youtube?.description}</Text>
+                </Box>
+                <Box width={'100%'}>
+                    <YoutubeIframe
+                        videoId={youtube?.id_youtube}
+                        height={250}
+                        onReady={() => setVideoReady(true)}
+                        onFullScreenChange={onFullScreenChange}
+                    />
+                    {!videoReady && <ActivityIndicator/>}
+                </Box>
+                <Box>
+                    <Button>{youtube?.title}</Button>
+                </Box>
+            </Box>
+        </ScrollView>
     );
 };
-
-const styles = StyleSheet.create({
-    center: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        textAlign: "center",
-    },
-});
-
 export default YoutubePage;
