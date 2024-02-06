e06eb518832291f5dbbc8be601ade95802cf984a
Thiago Tepê <ths.pereira@gmail.com>
2024-02-06 15:27:33 -0300

    Refactor Image component and improve readability
    The Image component has been refactored for increased clarity and readability. Unused imports from the 'native-base' library have been removed. The useEffect code has been slimmed down for readability. The Skeleton loading component has been made a separate variable for improved code structure.

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -1,4 +1,4 @@
-import {Image as NativeBaseImage, IImageProps, Skeleton, Text, HStack, VStack, Center, Box} from 'native-base';
+import {IImageProps, Skeleton, Box} from 'native-base';
 import React, {useContext, useEffect, useState} from 'react';
 import {Image as ImageRN} from "react-native";
 import ConfigContext from "../contexts/ConfigContext";
@@ -9,14 +9,16 @@ type Props = IImageProps & {
     height?: string | undefined;
 };
 
-export function Image({assetId = undefined, width = undefined, height = undefined, ...rest}: Props) {
+export function Image({assetId, width, height}: Props) {
     const {url_api, avatar_default} = useContext(ConfigContext);
     const [loading, setLoading] = useState(true);
-    const defaultImage = `${url_api}/assets/${avatar_default}?fit=cover`;
-    const [imageSrc, setImageSrc] = useState<string>(defaultImage);
+    const avatarPadrao = `${url_api}/assets/${avatar_default}?fit=cover`;
+    const [imageSrc, setImageSrc] = useState<string>(avatarPadrao);
 
     useEffect(() => {
-        const userImage = assetId ? `${url_api}/assets/${assetId}?fit=cover` : defaultImage;
+        const userImage = assetId
+            ? `${url_api}/assets/${assetId}?fit=cover`
+            : avatarPadrao;
         setImageSrc(userImage);
     }, [assetId, avatar_default, url_api]);
 
@@ -24,26 +26,30 @@ export function Image({assetId = undefined, width = undefined, height = undefine
         setLoading(false);
     };
 
+    // Extraindo o componente Skeleton como uma variável
+    const loadingSkeleton = (
+        <Box position={"absolute"} zIndex={9998} width={"100%"} height={height}>
+            <Skeleton
+                borderColor="coolGray.400"
+                endColor="text.400"
+                width={width}
+                height={height}
+                speed={2}
+            />
+        </Box>
+    );
+
     return (
         <Box position={"relative"}>
-            {loading && (
-                <Box position={"absolute"} zIndex={9998} width={'100%'} height={height}>
-                    <Skeleton
-                              borderColor="coolGray.400"
-                              endColor="text.400"
-                              width={width}
-                              height={height}
-                              speed={2}/>
-                </Box>
-            )}
-            <Box width={width} height={height} borderRadius={4}>
+            {loading && loadingSkeleton}
+            <Box width={width} height={height} borderRadius={"md"}>
                 <ImageRN
                     source={{uri: imageSrc}}
-                    style={{width: '100%', height: '100%'}}
+                    style={{width: "100%", height: "100%"}}
                     resizeMode="cover"
                     onLoadEnd={handleImageLoaded}
                 />
             </Box>
         </Box>
-    )
+    );
 }
