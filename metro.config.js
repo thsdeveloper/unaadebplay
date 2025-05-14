15c8fd1535116ddfb5a875874d6be1ac76ebf60e
ThiagoSoares <ths.pereira@gmail.com>
2025-05-14 18:38:46 -0300

    Atualizações de rotinas do metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,10 +1,11 @@
-const {
-  withNativeWind: withNativeWind
-} = require("nativewind/metro");
-
-const { getDefaultConfig } = require('expo/metro-config');
+// metro.config.js
+const { withNativeWind } = require("nativewind/metro");
+const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
+
+// Adicione suas personalizações extras aqui, se necessário
+config.resolver.sourceExts.push('cjs');
 
 module.exports = withNativeWind(config, {
   input: "./global.css"
